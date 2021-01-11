f915a675678c52fb1966a3ab07d64208ec6384d3
AnushaDammalapati <69410130+AnushaDammalapati@users.noreply.github.com>
2021-01-10 23:53:40 -0600

    Add files via upload

---
+++ MyCSharpGame/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MyCSharpGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.shooter = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Mygametimer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.shooter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shooter
+            // 
+            this.shooter.Image = global::MyCSharpGame.Properties.Resources.shooter;
+            this.shooter.Location = new System.Drawing.Point(527, 687);
+            this.shooter.Name = "shooter";
+            this.shooter.Size = new System.Drawing.Size(60, 65);
+            this.shooter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.shooter.TabIndex = 0;
+            this.shooter.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(916, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score: ";
+            // 
+            // Mygametimer
+            // 
+            this.Mygametimer.Interval = 25;
+            this.Mygametimer.Tick += new System.EventHandler(this.Timer_Event);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(461, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyCSharpGame.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1181, 755);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shooter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "My C# Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DownKey);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UpKey);
+            ((System.ComponentModel.ISupportInitialize)(this.shooter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox shooter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Mygametimer;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MyCSharpGame/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyCSharpGame
+{
+    public partial class Form1 : Form
+    {
+        bool shooterLeft, shooterRight;
+        int score = 0;
+        PictureBox[] rebel_list;
+        bool isGameEnded;
+        public Form1()
+        {
+            InitializeComponent();
+            GameStart();
+        }
+
+        private void Timer_Event(object sender, EventArgs e)
+        {
+            label1.Text = nameof(score)+": " + score;
+
+            if(shooterLeft)
+            {
+                shooter.Left = Math.Max(0, shooter.Left - 15);
+            }
+            if (shooterRight)
+            {
+                shooter.Left = Math.Min(845, shooter.Left + 15);
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "rebels")
+                {
+                    if (x.Top == shooter.Top - 40)
+                    {
+                        GameEnd("Game Over");
+                    }
+                    x.Top += 3;
+                    if (x.Bounds.IntersectsWith(shooter.Bounds))
+                     {
+                        GameEnd("Game Over");
+                     }
+                    foreach(Control y in this.Controls)
+                    {
+                        if (y is PictureBox && (string)y.Tag == "missile")
+                        {
+                            if(y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                x.Dispose();
+                                this.Controls.Remove(y);
+                                score = score + 10;
+                            }
+                        }
+                    }
+                }
+            if(x is PictureBox && (string)x.Tag == "missile")
+                {
+                    x.Top -= 20;
+                    if(x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            if(x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Top += 20;
+                    if(x.Top > 620)
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void DownKey(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                shooterLeft = true;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                shooterRight = true;
+            }
+        }
+
+        private void UpKey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                shooterLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                shooterRight = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                Missile();
+            }
+            if(e.KeyCode == Keys.Enter && isGameEnded == true)
+            {
+                GameReset();
+                GameStart();
+            }
+        }
+
+        private void Rebels()
+        {
+            RebelLine1();
+            RebelLine2();
+        }
+
+        private void RebelLine2()
+        {
+            rebel_list ??= new PictureBox[10];
+            int left = this.Left;
+            int top = this.Top;
+            for (int i = 0; i < rebel_list?.Length; i++)
+            {
+                rebel_list[i] = new PictureBox();
+                rebel_list[i].Size = new Size(60, 50);
+                rebel_list[i].Image = Properties.Resources.rebel;
+                rebel_list[i].Top = top + 120;
+                rebel_list[i].Tag = "rebels";
+                rebel_list[i].Left = left + 30;
+                rebel_list[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(rebel_list[i]);
+                left = left + 140;
+            }
+        }
+        private void RebelLine1()
+        {
+            rebel_list ??= new PictureBox[10];
+            int left = this.Left;
+            int top = this.Top;
+            for (int i = 0; i < rebel_list?.Length; i++)
+            {
+                rebel_list[i] = new PictureBox();
+                rebel_list[i].Size = new Size(60, 50);
+                rebel_list[i].Image = Properties.Resources.rebel;
+                rebel_list[i].Top = top + 20;
+                rebel_list[i].Tag = "rebels";
+                rebel_list[i].Left = left;
+                rebel_list[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(rebel_list[i]);
+                left = left + 140;
+            }
+        }
+
+        private void GameStart()
+        {
+            label1.Text = "Score: 0";
+            score = 0;
+            isGameEnded = false;
+            Rebels();
+            Mygametimer.Start();
+        }
+        private void GameEnd(string message)
+        {
+            isGameEnded = true;
+            Mygametimer.Stop();
+            label1.Text = "Score: " + score;
+            label2.Visible = true;
+            label2.Text = message;
+        }
+
+        private void GameReset()
+        {
+            foreach (PictureBox i in rebel_list)
+            {
+                this.Controls.Remove(i);
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((String)x.Tag == "missile" || (String)x.Tag == "rebel")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+        private void Missile()
+        {
+            PictureBox missile = new PictureBox();
+            missile = new PictureBox();
+            missile.Image = Properties.Resources.bullet1;
+            missile.Size = new Size(5, 20);
+            missile.Tag = "missile";
+            missile.Left = shooter.Left + shooter.Width / 2;
+            missile.Top = shooter.Top - 20;
+            this.Controls.Add(missile);
+            missile.BringToFront();
+        }
+    }
+}
